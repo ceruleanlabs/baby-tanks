a0595aaa0b67fd8dd663dce99f500bbdb13276e6
bergren2 <dan@heymrbass.com>
2014-07-19 17:45:33 -0500

    try something for Heroku

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var port = Number(process.env.PORT || 3000);
 var app = express();
 
-app.use('/', express.static(__dirname + '/dist'));
+// app.use('/', express.static(__dirname + '/dist'));
+app.get('/', function(req, res) {
+  res.send('Heroku, this better fucking work.');
+});
 
 app.listen(port, function() {
   console.log("Listening on " + port);
