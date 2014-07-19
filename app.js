186bb0ac30ae6dd61b57accde277a9adaf8effa5
bergren2 <dan@heymrbass.com>
2014-07-19 17:33:02 -0500

    specify root directory

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use('/', express.static(__dirname + '/dist'));
 
 app.listen(port, function() {
   console.log("Listening on " + port);
