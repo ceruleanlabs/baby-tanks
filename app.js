6075a14c8818583afffe5cbe2e19f671a987e37d
bergren2 <dan@heymrbass.com>
2014-07-19 17:39:52 -0500

    make sure port is a number

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var port = Number(process.env.PORT || 3000);
 var app = express();
 
 app.use('/', express.static(__dirname + '/dist'));
