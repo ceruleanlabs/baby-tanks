6a4c0fc672adbfcd51b1bbe5276358d108be0eba
bergren2 <dan@heymrbass.com>
2014-07-19 17:48:44 -0500

    revert FU Heroku changes

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var port = Number(process.env.PORT || 3000);
 var app = express();
 
-// app.use('/', express.static(__dirname + '/dist'));
-app.get('/', function(req, res) {
-  res.send('Heroku, this better fucking work.');
-});
+app.use('/', express.static(__dirname + '/dist'));
 
 app.listen(port, function() {
   console.log("Listening on " + port);
