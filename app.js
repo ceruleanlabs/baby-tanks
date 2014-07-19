f20ec62009185db5588da6294600157d6023771e
bergren2 <dan@heymrbass.com>
2014-07-19 17:54:23 -0500

    try something from SO

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
+process.env.PWD = process.cwd();
 var port = Number(process.env.PORT || 3000);
 var app = express();
 
-app.use('/', express.static(__dirname + '/dist'));
+app.use('/', express.static(process.env.PWD + '/dist'));
 
 app.listen(port, function() {
   console.log("Listening on " + port);
