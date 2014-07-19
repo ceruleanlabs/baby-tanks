eb3632560a98d7818d6a1cac2ade7611f9be6508
bergren2 <dan@heymrbass.com>
2014-07-19 15:20:19 -0500

    update tank

--- game/states/preload.js
+++ game/states/preload.js
@@ -13,7 +13,7 @@ Preload.prototype = {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
     this.load.image('yeoman', 'assets/baby-tank.jpg');
-    this.load.spritesheet('tank', 'assets/tank_sheet.png', 100, 80, 1);
+    this.load.spritesheet('tank', 'assets/tank_sheet.png', 100, 80, 2);
     this.load.image('ground', 'assets/grass.png');
     this.load.spritesheet('bullet', 'assets/tank_sheet.png', 100, 80, 1);
 
