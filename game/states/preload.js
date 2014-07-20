c7233e1f83f84a8521e36a7c18f1c46fadc97e4d
bergren2 <dan@heymrbass.com>
2014-07-19 21:33:25 -0500

    upgrade tank sprite

--- game/states/preload.js
+++ game/states/preload.js
@@ -14,7 +14,7 @@ Preload.prototype = {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
     this.load.image('baby-tank', 'assets/baby-tank.jpg');
-    this.load.spritesheet('tank', 'assets/tank_sheet.png', 100, 80, 2);
+    this.load.spritesheet('tank', 'assets/tank_sheet.png', 100, 80, 1);
     this.load.image('ground', 'assets/grass.png');
     this.load.image('block', 'assets/grassy.png');
     this.load.image('crosshair', 'assets/crosshair.png');
