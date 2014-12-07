3a1ff54d6a8a0eadd72c9f9a894bab94ce535ec2
Steve Pikelny <pikelnys@pikelnys-imac.blc-hq.com>
2014-12-06 18:19:50 -0600

    level 1

--- game/states/level1_1.js
+++ game/states/level1_1.js
@@ -30,8 +30,18 @@ Play.prototype = {
     this.flowers = this.game.add.sprite(0, this.game.height - 120, 'flowers');
 
     // Create/add the ground
-    this.ground = new Ground(this.game, 0, 500, 10000, 40);
-    this.game.add.existing(this.ground);
+    this.ground1 = new Ground(this.game, 0, 500, 1500, 40);
+    this.ground2 = new Ground(this.game, 950, 500, 450, 100);
+    this.ground3 = new Ground(this.game, 1800, 500, 900, 30);
+    this.ground4 = new Ground(this.game, 3000, 500, 1500, 40);
+    // this.ground5 = new Ground(this.game, 750, 500, 500, 60);
+    // this.ground6 = new Ground(this.game, 750, 500, 500, 60);
+    // this.ground7 = new Ground(this.game, 750, 500, 500, 60);
+    // this.ground8 = new Ground(this.game, 750, 500, 500, 60);
+    this.game.add.existing(this.ground1);
+    this.game.add.existing(this.ground2);
+    this.game.add.existing(this.ground3);
+    this.game.add.existing(this.ground4);
     //this.ground.body.setCollisionGroup(this.groundCG);
 
     // Create the tank
@@ -50,7 +60,17 @@ Play.prototype = {
     // this.game.add.existing(this.enemy);
 
     //moving enemy
-    this.movingEnemy = new MovingEnemy(this.game, 800, 300, 200);
+    this.movingEnemy = new MovingEnemy(this.game, 500, 300, 75);
+    this.game.add.existing(this.movingEnemy);
+    this.movingEnemy = new MovingEnemy(this.game, 900, 300, 200);
+    this.game.add.existing(this.movingEnemy);    
+    this.movingEnemy = new MovingEnemy(this.game, 1600, 300, 200);
+    this.game.add.existing(this.movingEnemy);
+    this.movingEnemy = new MovingEnemy(this.game, 1800, 300, 200, 400);
+    this.game.add.existing(this.movingEnemy);    
+    this.movingEnemy = new MovingEnemy(this.game, 1950, 300, 200,500);
+    this.game.add.existing(this.movingEnemy);
+    this.movingEnemy = new MovingEnemy(this.game, 2200, 300, 200);
     this.game.add.existing(this.movingEnemy);
 
     //health
@@ -58,7 +78,7 @@ Play.prototype = {
     // this.game.add.existing(this.healthPickup);
     // this.game.debug(this.healthPickup.sprite);
 
-    this.castle = new Castle(this.game, 1500, 330);
+    this.castle = new Castle(this.game, 2700, 330);
     this.game.add.existing(this.castle);
 
     // Camera
@@ -71,7 +91,7 @@ Play.prototype = {
     this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
   },
   update: function() {
-    if(this.tank.health <= 0) {
+    if(this.tank.health <= 0 || this.tank.position.y > 1000) {
       this.end_timer = this.game.time.events.add(Phaser.Timer.SECOND * 2, function () {
         this.game.state.start('next_level', true, false, 1, 1, false);
       }, this);
